5fa2a41b3b85126a9cbb3fd450cc14437263b3bb
Wicho <luiscalderonmiranda@gmail.com>
2022-07-16 20:42:19 -0500

    added welcome screen, Logo.jsx component and its styles, still doesn't quite look like the desire design

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Stack from 'react-bootstrap/Stack';
+import '../styles/logo.css';
+
+function Logo() {
+  return (
+    <>
+      <Stack gap={5} className='col-md-5 mx-auto'>
+        <img 
+          src={require('../imgs/logo.png')} 
+          alt='this is a logo' />
+        <h1 className='text'>TOCA LA PANTALLA</h1>
+      </Stack>
+    </>
+  );
+}
+
+export default Logo;
